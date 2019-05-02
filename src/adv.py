c3b62bde74f172b6f76c330764187a2bc887fbf4
matt0418 <mfeldma7@gmail.com>
2019-05-02 17:44:09 -0400

    had items working kinda, but messed it up

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -8,7 +9,7 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", Item('sword', 'pointy')),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -19,8 +20,11 @@ to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", Item('pokéflute', 'why not')),
 }
+
+
+
 
 
 # Link rooms together
@@ -36,10 +40,11 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
+
 #
 
 # Make a new player object that is currently in the 'outside' room.
-newPlayer = Player('Lols', room['outside'].name)
+newPlayer = Player('Lols', room['outside'])
 
 
 # Write a loop that:
@@ -67,23 +72,63 @@ def room_setup():
 def move_me(currentRoom, move):
     moving = move + '_to'
     newRoom = getattr(currentRoom, moving, False)
-    newPlayer.current_room = newRoom.name
-    print(newPlayer.current_room)
+    newPlayer.current_room = newRoom
+    # print(newPlayer.current_room)
     return newPlayer
 
 def start_game():
     in_treasure_room = False
     while in_treasure_room is False:
-        starting_room = room_setup()
-        new_move = input('Enter direction to go, [n] [s] [e] or [w] or [q] to quit the game')
-        if new_move == "q":
-            break
-        moved_player = move_me(starting_room, new_move)
-        if moved_player.current_room == 'Treasure Chamber':
-            in_treasure_room = True
+        try:
+            starting_room = room_setup()
+            new_move = input('Enter direction to go, [n] [s] [e] or [w] or [q] to quit the game')
+            if new_move == "q":
+                break
+            moved_player = move_me(starting_room, new_move)
+            if moved_player.current_room == 'Treasure Chamber':
+                in_treasure_room = True
+        except AttributeError:
+            print('This is not a valid entry, please try again')
 
-start_game()
-print('You won the game')
+def get_items(p):
+    print()
+    print(p.items.name)
+    print()
+    take_item = input("Do you want to pick up the item? [c] to confirm or [d] to deny")
+    if take_item == 'c':
+        p.lost_item(p.items)
+        print('Please continue going')
+    else:
+        print('Please continue in the direction you wish')
+        
+        
+
+print()
+print(newPlayer.current_room)    
+print("Enter a key to continue... \n [n]orth\n [s]outh\n [e]ast\n [w]est or \n [q]uit")
+while True:
+    print()
+    print()
+    cmd = input(' -> ')
+    if cmd == 'q':
+        break
+    elif cmd == 'n' or cmd == 's' or cmd == 'e' or cmd == 'w':
+        try:
+            updatedPlayer = move_me(newPlayer.current_room, cmd)
+            if updatedPlayer is not None:
+                print(f"{updatedPlayer.name} is in the {updatedPlayer.current_room.name}")
+                print(newPlayer.current_room.description)
+                print()
+                print(newPlayer.current_room.items)
+                if newPlayer.current_room.items is not None:
+                    get_items(newPlayer.current_room)
+        except AttributeError:
+            print('You broke the rules of the game, you are now dead')
+            break
+    else:
+        print('invalid entry')
+    # print(newPlayer.current_room.description)
+
     
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, storage=[]):
         self.name = name
         self.current_room = current_room
+        self.storage = storage
 
     def __str__(self):
         return str(f'{self.name}, {self.current_room}')
